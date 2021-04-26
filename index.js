75346dbb6b69f2de03ecfde1c404a0549c58689b
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-04-26 14:24:12 -0500

    updated functions in index js

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const list = document.getElementById("company-list")
 
-fetch("http://localhost:3000/companies")
-.then(response => response.json())
-.then(data => renderCompanies(data))
+const form = document.getElementById("company-form")
+const nameInput = document.getElementById("company-name")
+const descriptionInput = document.getElementById("company-description")
+const locationInput = document.getElementById("company-location")
+
+function getCompanies() {
+    
+    fetch("http://localhost:3000/companies")
+    .then(response => response.json())
+    .then(data => renderCompanies(data))
+
+}
 
 function renderCompanies(companyResponse) {
     const companies = companyResponse["data"]
     companies.forEach(company => {
-        const li = document.createElement("li")
-        li.innerText = `${company.attributes.name} based in ${company.attributes.location}`
-        list.append(li)
+        renderCompany(company)
     })
 }
+
+function renderCompany(company) {
+        const li = document.createElement("li")
+
+        li.innerHTML = `
+
+            <div>
+            <strong>${company.attributes.name}</strong>
+            <em>${company.attributes.location}:</em>
+            <span>${company.attributes.description}</span>
+            </div>
+
+            <button data-id="${company.id}">Edit</button>
+            <button data-id="${company.id}">Delete</button>
+            <br></br>
+
+        `
+    
+        list.appendChild(li)
+}
+
+getCompanies()
