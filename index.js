3c4dd30b5ff7edbe510ba15abdd890a06bad9d76
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-04-26 16:27:05 -0500

    updated handleFormSubmit function in index js

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+const baseURL = 'http://localhost:3000'
+const companiesURL = baseURL + '/companies'
+
 const list = document.getElementById("company-list")
 
 const form = document.getElementById("company-form")
 const nameInput = document.getElementById("company-name")
 const descriptionInput = document.getElementById("company-description")
 const locationInput = document.getElementById("company-location")
+
+form.addEventListener('submit', handleFormSubmit)
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    const formData = {
+        name: nameInput.value,
+        location: locationInput.value,
+        description: descriptionInput.value
+    }
+
+    const configObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    fetch(companiesURL, configObject)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+}
 
 function getCompanies() {
     
