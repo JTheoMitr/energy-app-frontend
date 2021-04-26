8a340b19720fa56e83ed88179302d34c4e66abf9
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-04-26 14:00:50 -0500

    updated index js file

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const list = document.getElementById("company-list")
+
 fetch("http://localhost:3000/companies")
 .then(response => response.json())
 .then(data => renderCompanies(data))
 
 function renderCompanies(companyResponse) {
-    debugger
+    const companies = companyResponse["data"]
+    companies.forEach(company => {
+        const li = document.createElement("li")
+        li.innerText = `${company.attributes.name} based in ${company.attributes.location}`
+        list.append(li)
+    })
 }
